c796c533ba298c8a6f317593633ec5cc16e3a4c1
Кирилл Курко <kirillkurko2609@gmail.com>
2020-10-22 12:59:09 +0300

    EmployeeInterface added

---
+++ src/main/java/model/beans/employees/EmployeeInterface.java
@@ -0,0 +1,32 @@
+package model.beans.employees;
+
+public interface EmployeeInterface {
+
+    int getId();
+
+    void setId(int id);
+
+    String getName();
+
+    void setName(String name);
+
+    String getSurname();
+
+    void setSurname(String surname);
+
+    int getExperience();
+
+    void setExperience(int experience);
+
+    double getSalary();
+
+    void setSalary(double salary);
+
+    int getUserID();
+
+    void setUserID(int userID);
+
+    int getProjectID();
+
+    void setProjectID(int projectID);
+}
