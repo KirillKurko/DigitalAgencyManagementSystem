84388efcd51669bcb7766af4736ac274f44d7b64
Кирилл Курко <kirillkurko2609@gmail.com>
2020-10-23 13:17:51 +0300

    Tool class implemented

---
+++ src/main/java/model/beans/employees/employeesUtilities/Tool.java
@@ -0,0 +1,47 @@
+package model.beans.employees.employeesUtilities;
+
+public class Tool {
+
+    private int id;
+    private String name;
+    private String description;
+
+    public Tool() {
+    }
+
+    public Tool(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Tool(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
+
